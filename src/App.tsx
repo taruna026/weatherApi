0b25360ec5ca4c2012955b20b743a6b6de817177
taruna.garg <taruna.garg@hashedin.com>
2020-05-16 00:07:09 +0530

    Landing Page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [cityName, setCity] = React.useState('');
+  const [data, setData] = React.useState(null);
+
+  const onCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+};
+const getCityData=(event:any)=>{
+  event.preventDefault();
+        fetch('https://samples.openweathermap.org/data/2.5/forecast/hourly?q=&appid=b0069842425823d7a53daf72c6eb7429'+cityName)
+            .then(async(res) =>{
+                let ans = await res.json()
+                console.log(ans)
+            })
+}
+const isEnabled = cityName.length>0;
+
   return (
     <div className="App">
-      Hello World!!
+        <form onSubmit={getCityData}>
+          <input type="text" placeholder="Enter a city name" onChange={onCityChange} value={cityName} className="input-box"/>
+          <div><button className="btn" type="submit" disabled={!isEnabled}>Get the Forecast</button></div>
+        </form>
     </div>
   );
 }
